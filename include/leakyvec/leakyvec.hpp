c95e3926c174df490648273d114387553e349b41
Austin Gill <Notgnoshi@gmail.com>
2025-11-30 11:03:52 -0600

    Add leaky::Vec wrapper

---
+++ include/leakyvec/leakyvec.hpp
@@ -0,0 +1,42 @@
+#pragma once
+#include <vector>
+
+namespace leaky {
+
+/// @brief a wrapper around std::vector that allows leaking its contents and transfering ownership
+///
+/// @tparam T the element type
+/// @tparam Alloc the allocator type
+template<typename T, typename Alloc = typename std::vector<T>::allocator_type>
+class Vec
+{
+  private:
+    std::vector<T, Alloc> m_inner;
+
+  public:
+    /// @brief Create a leaky Vec from a std::vector. Takes exclusive ownership.
+    Vec(std::vector<T, Alloc>&& vec) noexcept : m_inner(std::move(vec)) {}
+    /// @brief Take exclusive ownership from another leaky Vec.
+    Vec(Vec&& other) noexcept : m_inner(std::move(other.m_inner)) {}
+    /// @brief A Vec owns the internal vector exclusively; no copying.
+    Vec(const Vec&) = delete;
+    ~Vec() noexcept = default;
+
+    Vec& operator=(const Vec&) = delete;
+    Vec& operator=(Vec&& other) noexcept
+    {
+        m_inner = std::move(other.m_inner);
+        return *this;
+    }
+
+    /// @brief Take ownership of the std::vector back
+    std::vector<T, Alloc> take() noexcept { return std::move(m_inner); }
+    /// @brief Get a const reference to the internal std::vector
+    const std::vector<T, Alloc>& as_ref() const noexcept { return m_inner; }
+    /// @brief Get a mutable reference to the internal std::vector
+    std::vector<T, Alloc>& as_mut() noexcept { return m_inner; }
+
+    /// @brief Get the allocator that owns the vector's memory
+    constexpr Alloc get_allocator() const noexcept { return m_inner.get_allocator(); }
+};  // class Vec
+}  // namespace leaky
